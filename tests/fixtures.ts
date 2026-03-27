02297679ea8141b3ab7418c4e3e074873bdbd219
Zack Leya <zleya96@gmail.com>
2026-03-27 12:41:12 -0400

    Added fixtures for todo page

---
+++ tests/fixtures.ts
@@ -0,0 +1,15 @@
+import {test as base} from "@playwright/test";
+import { ToDoPage } from "../pages/todo-page";
+
+
+type Fixtures = {
+    tdPage: ToDoPage
+}
+export const test = base.extend<Fixtures>({
+    tdPage: async ({ page }, use) => {
+        const tdPage = new ToDoPage(page);
+        await tdPage.openToDoDemo();
+        await use(tdPage);
+        await page.close();
+    }
+})
--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -1,51 +1,26 @@
-import {test, expect} from "@playwright/test"; // <---- What we downloaded when we initialized playwright project. Under dir node_modules
+import {test} from "../tests/fixtures.ts"; // <---- What we downloaded when we initialized playwright project. Under dir node_modules
 import { ToDoPage } from "../pages/todo-page";
+import { expect } from "@playwright/test";
 
 
 //tests take in two parameters: 1. Title of test 2. Function
-//Syntax
 
-/*
-test("title", ()=> {
-
-    //step 1
-    //step 2
-    //step 3
-
-})
-    */
-
-                            // {page} <-- called a fixture. AKA --> Global Variable
-// test("Verify Page Title", async ({page})=>{
-//     const toDoPage = new ToDoPage(page);
-
-//     toDoPage.openToDoDemo();
-//     const url = await page.url();
-//     console.log("URL:", url);
-
-//     const title = toDoPage.getToDoTitle(page);
-//     console.log("Title = ",title);
-
-//     await expect(page).toHaveTitle(/React • TodoMVC/); // <----one assertion per test
-
-// });
-
-test("Verify ToDo", async ({page})=>{
-    const toDoPage = new ToDoPage(page);
+test("Verify ToDo", async ({page, tdPage})=>{
+    
     const todoA: string = "Pet Marvin";
     const todoB: string = "Pet Clem";
 
-    await toDoPage.openToDoDemo();
+    // await tdPage.openToDoDemo();
 
     //Create ToDo_A
-    await toDoPage.addToDo(todoA);
-    await toDoPage.addToDo(todoB);
+    await tdPage.addToDo(todoA);
+    await tdPage.addToDo(todoB);
     
     //Delete ToDo_A
-    await toDoPage.deleteToDo(todoA);
+    await tdPage.deleteToDo(todoA);
 
     //Verify ToDo_A Deleted
-    await expect(page.locator(toDoPage.getLabelPath(todoA))).toBeHidden;
+    await expect(page.locator(tdPage.getLabelPath(todoA))).toBeHidden;
 
 
     //Complete ToDoB
