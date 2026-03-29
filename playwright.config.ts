b36a8a849c8e6e2495ac0d33feb6f2c723549309
Zack Leya <zleya96@gmail.com>
2026-03-29 12:55:49 -0400

    Sharding support

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'], ['github'], ['blob']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
   // 1. Test timeout: Time for a single test (30s default)
